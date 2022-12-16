1b1283a5dc98110a34b648bd0274094fe8a02feb
Jonas22rr <77532884+Jonas22rr@users.noreply.github.com>
2022-12-16 09:31:05 +0000

    add verify

---
+++ worktime-tracker-dev/components/account/verify.tsx
@@ -0,0 +1,11 @@
+export default function verify() {
+    return (
+        <div className="flex flex-col pt-10 pr-52">
+            <span className="text-3xl">Account Created</span>
+            <span className="text-xl pt-5">You're one step closer to capturing your time more easily.</span>
+            <span className="text-lg">You just need to verify your email address so we can make sure you are a real person. <br/> We do this to prevent fake accounts. <br/><br/>
+                        We have sent you an email with the verification link. You did not receive the link? Let us try to <a href="" className="text-color-reg-now">send it again</a> .
+            </span>
+        </div>
+    )
+}
