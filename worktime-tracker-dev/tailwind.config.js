b91bc0b5690b1ae3ec7d85029de727e59cf03d60
Jonas22rr <77532884+Jonas22rr@users.noreply.github.com>
2022-12-16 11:29:30 +0000

    revise tailwind config file

--- worktime-tracker-dev/tailwind.config.js
+++ worktime-tracker-dev/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         'bg-to': '#3CC083',
         'bg-base': '#171717',
         'color-reg-now': '#00BFA6',
+        'account-input': '#262626',
       },
 	  width: {
 		'account-input':'360px'
