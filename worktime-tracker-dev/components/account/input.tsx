2fc42993de7a03c031f9ae5baad8551bf62f2258
Jonas22rr <77532884+Jonas22rr@users.noreply.github.com>
2022-12-16 11:28:41 +0000

    add input component

---
+++ worktime-tracker-dev/components/account/input.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+interface Props {
+    inputType: string,
+    inputValue: string,
+    setInputValue: (value: string) => void,
+    required: boolean
+}
+export default function accountInput (props: Props) {
+    const handleChangeValue = (event: any) => {
+        if (event.target.value.length !== null) {
+            props.setInputValue(event.target.value)
+        }
+    }
+
+    return (
+        <>
+            <input type={props.inputType} value={props.inputValue} onChange={(event: any) => handleChangeValue(event)} className="bg-account-input h-10 w-account-input pl-2 rounded-md outline-none" required={props.required}/>
+        </>
+    )
+}
