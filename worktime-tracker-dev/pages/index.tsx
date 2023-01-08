ccd43988e0615c07d01feb1ae0d42d85d10a0b54
Jonas22rr <jonasbott2@web.de>
2023-01-08 16:24:59 +0100

    test navbar

--- worktime-tracker-dev/pages/index.tsx
+++ worktime-tracker-dev/pages/index.tsx
@@ -1,5 +1,9 @@
 import NavBar from "../components/NavBar";
 
 export default function Index() {
-    return <div className="bg-bg-base h-screen w-screen">Home</div>;
+    return (
+        <div className="bg-bg-base h-screen w-screen">
+            <NavBar />
+        </div>
+    );
 }
