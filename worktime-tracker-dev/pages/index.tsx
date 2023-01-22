ccc889678237e7eb0ec0d86df04220b180440f83
Jonas22rr <jonasbott2@web.de>
2023-01-22 20:11:00 +0100

    add Navbar to index

--- worktime-tracker-dev/pages/index.tsx
+++ worktime-tracker-dev/pages/index.tsx
@@ -1,3 +1,9 @@
+import NavBar from "../components/navigation/navBarTracking";
+
 export default function Index() {
-    return <div className="bg-bg-base h-screen w-screen"></div>;
+    return (
+        <div className="bg-bg-base h-screen w-screen">
+            <NavBar />
+        </div>
+    );
 }
