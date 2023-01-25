f70f61950512859c8cf279effca892774ce7b0bc
Jonas22rr <jonasbott2@web.de>
2023-01-25 21:18:44 +0100

    add Timebox for analyses

---
+++ worktime-tracker-dev/components/analyses/TimeBox.tsx
@@ -0,0 +1,15 @@
+import { ITimeBoxProps as Props } from "../../lib/types/types";
+
+export default function TimeBox(props: Props) {
+    return (
+        // <div
+        //     className={`bg-account-input py-5 pl-4 pr-64 flex flex-col rounded-lg ${props.style} `}
+        // >
+        <div
+            className={`bg-account-input py-5 pl-4 flex flex-col rounded-lg ${props.style} `}
+        >
+            <span className="text-[#818181]">{props.average}</span>
+            <span className="text-[#818181] text-4xl">{props.time}</span>
+        </div>
+    );
+}
