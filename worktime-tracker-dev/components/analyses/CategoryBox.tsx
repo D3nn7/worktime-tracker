b3fbfcc795ec7be5a0d6e80e45d570e90dd0d007
Jonas22rr <jonasbott2@web.de>
2023-01-29 18:11:32 +0100

    implement donut-chart

--- worktime-tracker-dev/components/analyses/CategoryBox.tsx
+++ worktime-tracker-dev/components/analyses/CategoryBox.tsx
@@ -1,5 +1,6 @@
 import { ICategoryBoxProps as Props } from "../../lib/types/types";
 import Dropdown from "./Dropdown";
+import { DonutChart } from "react-circle-chart";
 
 export default function CategoryBox(props: Props) {
     return (
@@ -7,6 +8,20 @@ export default function CategoryBox(props: Props) {
             <div className="flex flex-row content-center items-center ">
                 <span className="text-[#818181] m-5">{props.average}</span>
                 <Dropdown />
+            </div>
+            <div className="w-full flex flex-col justify-center items-center">
+                <DonutChart
+                    items={[
+                        { value: 30, label: "Logistik", color: "#00BFA6" },
+                        { value: 25, label: "Fachbereich" },
+                        { value: 45, label: "IT" },
+                    ]}
+                    size={280}
+                    backgroundTooltipColor="#303030"
+                    showTotal={false}
+                    totalTextColor="aliceblue"
+                    tooltipSx={{ fontSize: "1.2rem", borderRadius: "8px" }}
+                />
             </div>
         </div>
     );
