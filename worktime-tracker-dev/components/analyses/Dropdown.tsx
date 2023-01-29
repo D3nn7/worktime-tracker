e4254fc8951f12e38a2118cb9f89808bef434c2c
Jonas22rr <jonasbott2@web.de>
2023-01-29 20:20:36 +0100

    revise path properties

--- worktime-tracker-dev/components/analyses/Dropdown.tsx
+++ worktime-tracker-dev/components/analyses/Dropdown.tsx
@@ -32,9 +32,9 @@ export default function Dropdown() {
                     xmlns="http://www.w3.org/2000/svg"
                 >
                     <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M19 9l-7 7-7-7"
                     ></path>
                 </svg>
