a2a4ad8503ae5da45496d7352e4fe2f2a951a037
Jonas <j.bott@g-d-c.eu>
2023-02-13 11:22:35 +0100

    add logo

--- worktime-tracker-dev/components/history/NoTrackedTimes.tsx
+++ worktime-tracker-dev/components/history/NoTrackedTimes.tsx
@@ -1,7 +1,17 @@
+import Logo from "../../public/static/addNotesLogo.svg";
+import Image from "next/image";
+
 export default function NoTrackedTimes() {
     return (
         <div className="flex flex-col place-items-center justify-center h-screen w-screen">
-            <span className="text-4xl text-[#818181] font-bold ">
+            <Image
+                src={Logo}
+                alt="NoTrackedTimes"
+                height={350}
+                width={350}
+                priority
+            />
+            <span className="text-4xl pt-20 text-[#818181] font-bold ">
                 Uh... There are no tracked times yet.
             </span>
             <span className="text-2xl text-[#818181] pt-3">
