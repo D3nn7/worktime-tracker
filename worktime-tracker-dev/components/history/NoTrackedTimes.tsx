33481b2a635c9492ad704baa7aef49252a06c9b0
Jonas <j.bott@g-d-c.eu>
2023-02-13 11:02:17 +0100

    add NoTrackedTimes in history

---
+++ worktime-tracker-dev/components/history/NoTrackedTimes.tsx
@@ -0,0 +1,12 @@
+export default function NoTrackedTimes() {
+    return (
+        <div className="flex flex-col place-items-center justify-center h-screen w-screen">
+            <span className="text-4xl text-[#818181] font-bold ">
+                Uh... There are no tracked times yet.
+            </span>
+            <span className="text-2xl text-[#818181] pt-3">
+                Start tracking to see here amazing stuff happening.
+            </span>
+        </div>
+    );
+}
--- worktime-tracker-dev/pages/tracking/history.tsx
+++ worktime-tracker-dev/pages/tracking/history.tsx
@@ -1,70 +1,55 @@
 import NavBar from "../../components/navigation/NavBarTracking";
-import HistoryBox from "../../components/history/HistoryBox";
+import NoTrackedTimes from "../../components/history/NoTrackedTimes";
 import { IHistoryBoxProps } from "../../lib/types/types";
-import NoEntryBox from "../../components/NoEntryBox";
+import HistoryIndex from "../../components/history/Index";
 
 export default function History() {
     const histories = [
-        {
-            description: "Add LinkedIn Post",
-            category: "Marketing",
-            timeSum: "00:38:15",
-            duration: "06.12.2022 15:23 PM - 06.12.2022 15:50 PM",
-        },
-        {
-            description: "Add LinkedIn Post",
-            category: "Marketing",
-            timeSum: "00:38:15",
-            duration: "06.12.2022 15:23 PM - 06.12.2022 15:50 PM",
-        },
-        {
-            description: "Add LinkedIn Post",
-            category: "Marketing",
-            timeSum: "00:38:15",
-            duration: "06.12.2022 15:23 PM - 06.12.2022 15:50 PM",
-        },
-        {
-            description: "Add LinkedIn Post",
-            category: "Marketing",
-            timeSum: "00:38:15",
-            duration: "06.12.2022 15:23 PM - 06.12.2022 15:50 PM",
-        },
-        {
-            description: "Add LinkedIn Post",
-            category: "Marketing",
-            timeSum: "00:38:15",
-            duration: "06.12.2022 15:23 PM - 06.12.2022 15:50 PM",
-        },
-        {
-            description: "Add LinkedIn Post",
-            category: "Marketing",
-            timeSum: "00:38:15",
-            duration: "06.12.2022 15:23 PM - 06.12.2022 15:50 PM",
-        },
+        // {
+        //     description: "Add LinkedIn Post",
+        //     category: "Marketing",
+        //     timeSum: "00:38:15",
+        //     duration: "06.12.2022 15:23 PM - 06.12.2022 15:50 PM",
+        // },
+        // {
+        //     description: "Add LinkedIn Post",
+        //     category: "Marketing",
+        //     timeSum: "00:38:15",
+        //     duration: "06.12.2022 15:23 PM - 06.12.2022 15:50 PM",
+        // },
+        // {
+        //     description: "Add LinkedIn Post",
+        //     category: "Marketing",
+        //     timeSum: "00:38:15",
+        //     duration: "06.12.2022 15:23 PM - 06.12.2022 15:50 PM",
+        // },
+        // {
+        //     description: "Add LinkedIn Post",
+        //     category: "Marketing",
+        //     timeSum: "00:38:15",
+        //     duration: "06.12.2022 15:23 PM - 06.12.2022 15:50 PM",
+        // },
+        // {
+        //     description: "Add LinkedIn Post",
+        //     category: "Marketing",
+        //     timeSum: "00:38:15",
+        //     duration: "06.12.2022 15:23 PM - 06.12.2022 15:50 PM",
+        // },
+        // {
+        //     description: "Add LinkedIn Post",
+        //     category: "Marketing",
+        //     timeSum: "00:38:15",
+        //     duration: "06.12.2022 15:23 PM - 06.12.2022 15:50 PM",
+        // },
     ] as IHistoryBoxProps[];
     return (
         <div>
             <NavBar />
-            <div className="container mx-auto pt-40 pb-20 ">
-                <div className="text-3xl ">History</div>
-                <div className="space-y-5 pt-10">
-                    {histories.map((history, index) => {
-                        return (
-                            <HistoryBox
-                                key={index}
-                                category={history.category}
-                                description={history.description}
-                                timeSum={history.timeSum}
-                                duration={history.duration}
-                            />
-                        );
-                    })}
-                    <NoEntryBox
-                        category="No more tracked times here."
-                        description="Start tracking to see more here."
-                    />
-                </div>
-            </div>
+            {histories.length === 0 ? (
+                <NoTrackedTimes />
+            ) : (
+                <HistoryIndex histories={histories} />
+            )}
         </div>
     );
 }
