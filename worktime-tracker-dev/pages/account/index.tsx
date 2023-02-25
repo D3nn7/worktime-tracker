dfa6f166aa8192a613bf5437182f8db18093ec46
Jonas22rr <jonasbott2@web.de>
2023-02-25 19:02:28 +0100

    revise input fields

--- worktime-tracker-dev/pages/account/index.tsx
+++ worktime-tracker-dev/pages/account/index.tsx
@@ -26,7 +26,7 @@ export default function Account() {
                                     <input
                                         type="text"
                                         id="idName"
-                                        className="bg-account-input h-10 w-account-input pl-2 rounded-md outline-none"
+                                        className="bg-account-input h-10 w-account-input pl-2 rounded-md outline-none border-[1px] border-[#505358] focus:border-cyan-base"
                                     />
                                 </div>
                                 <div className="flex flex-col pb-5">
@@ -37,14 +37,14 @@ export default function Account() {
                                         type="email"
                                         id="idMail"
                                         name="mail"
-                                        className="bg-account-input h-10 w-account-input pl-2 rounded-md outline-none"
+                                        className="bg-account-input h-10 w-account-input pl-2 rounded-md outline-none border-[1px] border-[#505358] focus:border-cyan-base"
                                         required
                                     />
                                 </div>
                                 <div className="flex flex-col w-20">
                                     <button
                                         type="button"
-                                        className="bg-cyan-base hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0"
+                                        className="bg-cyan-base hover:bg-teal-600 focus:ring-0 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0"
                                     >
                                         Save
                                     </button>
